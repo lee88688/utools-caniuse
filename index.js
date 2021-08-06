806a529531199fa0004027a4a1e44e06a1862403
lee <lee88688@163.com>
2021-08-06 10:56:03 +0800

    caniuse搜索，将caniuse网页集成到utools中，方便快捷使用。

---
+++ index.js
@@ -0,0 +1,25 @@
+let searchValue = '';
+
+const iframe = document.getElementById('content');
+window.f = iframe;
+
+function debounce(fn, ms) {
+  let timer = null;
+  return (...args) => {
+    if (timer) {
+      clearTimeout(timer);
+      timer = null;
+    }
+    timer = setTimeout(() => fn(...args), ms);
+  }
+}
+
+const changeSearch = debounce(({text}) => {
+  if (text) {
+    iframe.src = `https://caniuse.com/?search=${text}`
+  }
+}, 500);
+
+utools.onPluginReady(() => {
+  utools.setSubInput(changeSearch, '', true);
+});
